09098f09ff6139df9f5c2d7f6997d0d677d4a30e
Bhat Junaid <junaidbhat110@gmail.com>
2019-09-11 15:26:09 +0530

    Added new version on HDFC

--- routes/index.js
+++ routes/index.js
@@ -520,57 +520,107 @@ router.post('/api/extractData', function (req, res) {
           }
           resData.assetsArray = assetsArray
         } else if (brokerType == 6) {
-          let indexBroker = 1 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'CONTRACT NOTE '
-          })
-          let indexDate = 2 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Clearing / Trading No.'
-          })
-          let indexCnnum = 1 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Clearing / Trading No.'
-          })
+
+          let indexBroker, indexDate, indexCnnum, sttIndex, stIndex = 0, cgstIndex, sgstIndex, transIndex, stTransIndex, otherIndex, stOtherIndex, stampIndex, totalIndex
+
+          if (data[0].str == 'CONTRACT NOTE CUM TAX INVOICE') {
+            indexBroker = 3 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'CONTRACT NOTE CUM TAX INVOICE'
+            })
+            indexDate = 6 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Invoice No.'
+            })
+            indexCnnum = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Invoice No.'
+            })
+
+            sttIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Securities Transaction Tax (Rs.)'
+            })
+
+            cgstIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'CGST Amount (Rs.) *'
+            })
+
+            sgstIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'SGST Amount (Rs.) *'
+            })
+
+            igstIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'IGST Amount (Rs.) *'
+            })
+
+            transIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Rs.) #'
+            })
+
+            otherIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Taxable value of supply (SEBI turnover Fees) (Rs.)'
+            })
+
+            stampIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Stamp Duty (Rs.)'
+            })
+
+            totalIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Net amount receivable by Client / (payable by Client) (Rs.)'
+            })
+          } else {
+            indexBroker = 2 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'CONTRACT NOTE '
+            })
+            indexDate = 2 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Clearing / Trading No.'
+            })
+            indexCnnum = 1 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Clearing / Trading No.'
+            })
+
+            sttIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Securities  Transaction Tax (Rs.)'
+            })
+
+            stIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Service Tax (15% of Brokerage) (Rs.)**'
+            })
+
+            transIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Exchange Transaction Charges (Rs.)'
+            })
+
+            stTransIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Service Tax on Exchange Transaction Charges (Rs.)'
+            })
+
+            otherIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'SEBI turnover Fees (Rs.)'
+            })
+
+            stOtherIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Service Tax on SEBI turnover Fees (Rs.)'
+            })
+
+            stampIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Stamp Duty (Rs.)'
+            })
+
+            totalIndex = 5 + _.findIndex(data, function (obj) {
+              return obj.str.trim() == 'Net amount receivable by Client /(payable by Client) (Rs.)'
+            })
+          }
 
           resData.date = data[indexDate].str.trim()
           resData.broker = data[indexBroker].str.trim()
           resData.cnnum = data[indexCnnum].str.trim()
-
-          let sttIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Securities  Transaction Tax (Rs.)'
-          })
-
-          let stIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Service Tax (15% of Brokerage) (Rs.)**'
-          })
-
-          let transIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Exchange Transaction Charges (Rs.)'
-          })
-
-          let stTransIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Service Tax on Exchange Transaction Charges (Rs.)'
-          })
-
-          let otherIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'SEBI turnover Fees (Rs.)'
-          })
-
-          let stOtherIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Service Tax on SEBI turnover Fees (Rs.)'
-          })
-
-          let stampIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Stamp Duty (Rs.)'
-          })
-
-          let totalIndex = 5 + _.findIndex(data, function (obj) {
-            return obj.str.trim() == 'Net amount receivable by Client /(payable by Client) (Rs.)'
-          })
-
           resData.stt = Number(data[sttIndex].str.trim())
           resData.stamp = Number(data[stampIndex].str.trim())
           resData.other = Number(data[otherIndex].str.trim())
           resData.trans = Number(data[transIndex].str.trim())
-          resData.gst = Number(data[stIndex].str.trim()) + Number(data[stOtherIndex].str.trim()) + Number(data[stTransIndex].str.trim())
+          if (stIndex != 0) {
+            resData.gst = Number(data[stIndex].str.trim()) + Number(data[stOtherIndex].str.trim()) + Number(data[stTransIndex].str.trim())
+          } else {
+            resData.gst = Number(data[cgstIndex].str.trim()) + Number(data[sgstIndex].str.trim()) + Number(data[igstIndex].str.trim())
+          }
           resData.total = data[totalIndex].str.trim()
 
           let startIndex = 2 + _.findIndex(data, function (obj) {
